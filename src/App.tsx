211435b4a3e3c1a4032193350f7de53af02d2c98
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-10-25 17:35:50 +0300

    feat: create MainRouter and MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import './App.scss';
 
-import Footer from '@src/components/Footer/Footer';
-import Header from '@src/components/Header/Header';
 import routes from '@src/constants/routes';
-import Account from '@src/pages/Account/Account';
-import Actors from '@src/pages/Actors/Actors';
-import DetailedActor from '@src/pages/DetailedActor/DetailedActor';
-import DetailedDirector from '@src/pages/DetailedDirector/DetailedDirector';
-import DetailedMovie from '@src/pages/DetailedMovie/DetailedMovie';
-import Directors from '@src/pages/Directors/Directors';
-import Favorites from '@src/pages/Favorites/Favorites';
 import Login from '@src/pages/Login/Login';
-import Movies from '@src/pages/Movies/Movies';
 import Registration from '@src/pages/Registration/Registration';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import MainRouter from './components/MainRouter/MainRouter';
 import useAppSelector from './hooks/useAppSelector';
 
 function App() {
@@ -22,44 +13,18 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/*" element={<Navigate to={routes.movies} replace />} />
-          <Route path={routes.movies} element={<Movies />} />
-          <Route path={routes.actors} element={<Actors />} />
-          <Route path={routes.directors} element={<Directors />} />
-          <Route path={routes.detailedMovie} element={<DetailedMovie />} />
-          <Route path={routes.detailedActor} element={<DetailedActor />} />
-          <Route
-            path={routes.detailedDirector}
-            element={<DetailedDirector />}
-          />
-          <Route
-            path={routes.account}
-            element={
-              isAuth ? <Account /> : <Navigate to={routes.registration} />
-            }
-          />
-          <Route
-            path={routes.favorites}
-            element={
-              isAuth ? <Favorites /> : <Navigate to={routes.registration} />
-            }
-          />
-          <Route
-            path={routes.login}
-            element={!isAuth ? <Login /> : <Navigate to={routes.movies} />}
-          />
-          <Route
-            path={routes.registration}
-            element={
-              !isAuth ? <Registration /> : <Navigate to={routes.movies} />
-            }
-          />
-        </Routes>
-      </main>
-      <Footer />
+      <Routes>
+        <Route path="/*" element={<MainRouter />} />
+
+        <Route
+          path={routes.login}
+          element={!isAuth ? <Login /> : <Navigate to={routes.movies} />}
+        />
+        <Route
+          path={routes.registration}
+          element={!isAuth ? <Registration /> : <Navigate to={routes.movies} />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/components/MainRouter/MainRouter.tsx
@@ -0,0 +1,49 @@
+import routes from '@src/constants/routes';
+import useAppSelector from '@src/hooks/useAppSelector';
+import MainLayout from '@src/layouts/MainLayout/MainLayout';
+import Account from '@src/pages/Account/Account';
+import Actors from '@src/pages/Actors/Actors';
+import DetailedActor from '@src/pages/DetailedActor/DetailedActor';
+import DetailedDirector from '@src/pages/DetailedDirector/DetailedDirector';
+import DetailedMovie from '@src/pages/DetailedMovie/DetailedMovie';
+import Directors from '@src/pages/Directors/Directors';
+import Favorites from '@src/pages/Favorites/Favorites';
+import Movies from '@src/pages/Movies/Movies';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+const MainRouter = () => {
+  const isAuth = useAppSelector((state) => state.userReducer.isAuthorized);
+
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/*" element={<Navigate to={routes.movies} replace />} />
+          <Route path={routes.movies} element={<Movies />} />
+          <Route path={routes.actors} element={<Actors />} />
+          <Route path={routes.directors} element={<Directors />} />
+          <Route path={routes.detailedMovie} element={<DetailedMovie />} />
+          <Route path={routes.detailedActor} element={<DetailedActor />} />
+          <Route
+            path={routes.detailedDirector}
+            element={<DetailedDirector />}
+          />
+          <Route
+            path={routes.account}
+            element={
+              isAuth ? <Account /> : <Navigate to={routes.registration} />
+            }
+          />
+          <Route
+            path={routes.favorites}
+            element={
+              isAuth ? <Favorites /> : <Navigate to={routes.registration} />
+            }
+          />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default MainRouter;
